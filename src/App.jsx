920462a0ab2d6bd4c90334fba4d7ac8e77fc9dc4
omin-h <ominhemapriya14@gmail.com>
2025-11-11 11:34:31 +0530

    refactor: Remove SuperscriptInput component and clean up imports in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-// import SuperscriptInput from './SuperscriptInput';
+
 import SecondPaper from "./secondPaper";
-// import FractionCanvas from "./fractionCanvas/fractionCanvas";
+import FractionCanvas from "./fractionCanvas/fractionCanvas";
 
 function App() {
   return (
     <>
-      {/* <SuperscriptInput /> */}
+     
       {/* <FractionCanvas /> */}
  
       <SecondPaper />
